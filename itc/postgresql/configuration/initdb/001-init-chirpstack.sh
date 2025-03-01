2f87ac086ad62e46444af40692676afaf295182c
disk91 <disk_91@homtail.com>
2025-03-01 14:56:32 +0100

    Common - executable scripts + config update to boot container

--- itc/postgresql/configuration/initdb/001-init-chirpstack.sh
+++ itc/postgresql/configuration/initdb/001-init-chirpstack.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role itc with login password '$PSQL_ITC_PASSWORD';
-    create database itc with owner itc;
+    create role ${COMMON_PSQL_USER} with login password '${COMMON_PSQL_PASSWORD}';
+    create database itc with owner ${COMMON_PSQL_USER};
 EOSQL

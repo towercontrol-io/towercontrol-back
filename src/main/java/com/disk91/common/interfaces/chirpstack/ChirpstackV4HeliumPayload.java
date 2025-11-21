05ec197d6821e72fd801603ee947fe94957f32a0
Paul <disk_91@hotmail.com>
2025-11-21 17:24:23 +0100

    Common - that's more helium and for v4, consider this can evolve and we need to have different loaders

--- src/main/java/com/disk91/common/interfaces/chirpstack/ChirpstackV4HeliumPayload.java
+++ src/main/java/com/disk91/common/interfaces/chirpstack/ChirpstackV4HeliumPayload.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ChirpstackPayload {
+public class ChirpstackV4HeliumPayload {
 
     /*
     {
